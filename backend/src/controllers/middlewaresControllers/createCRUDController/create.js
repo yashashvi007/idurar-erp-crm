ba8c84f30c5a9c81769789ecd3d7325c17a02727
Yashashvi Maurya <yashashvimaurya@gmail.com>
2024-04-23 08:01:13 +0530

    fixed duplicate product and product category creation

--- backend/src/controllers/middlewaresControllers/createCRUDController/create.js
+++ backend/src/controllers/middlewaresControllers/createCRUDController/create.js
@@ -1,5 +1,11 @@
 const create = async (Model, req, res) => {
   // Creating a new document in the collection
+  if (req.route.path.includes("product") && await Model.find({ name: req.body.name })) {
+    return res.status(400).json({
+      success: false,
+      message: `${req.route.path.split('/')[1]} already exists`,
+    });
+  }
   req.body.removed = false;
   const result = await new Model({
     ...req.body,
